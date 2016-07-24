7b6b42ae3e95c59db46980a4468a24e8f618cfb4
kent42ilink <kent42.ilink@gmail.com>
2016-07-24 15:51:16 +0800

    加入專案檔案。

---
+++ CordovaApp/www/scripts/OpenData.js
@@ -0,0 +1,41 @@
+﻿function getJson() {
+
+    var queryString = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=55ec6d6e-dc5c-4268-a725-d04cc262172b"
+
+    $.getJSON(queryString, function (Data) {
+        showData(Data);
+
+    }).fail(function (jqXHR) {
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. " + jqXHR.statusText);
+    });
+
+    return false;
+}
+
+function showData(Data) {
+
+    if (Data.length != 0 && Data.result.length != 0) {
+
+        $('#error-msg').hide();
+        $('#deviceready').show();
+
+        var tbl = $("#tableData tbody");
+
+        $.each(Data.result.results, function (i, item) {
+
+            tbl.append("<tr><td>" + item._id + "</td><td>"
+                + item.Station + "</td><td>" + item.Destination
+                + "</td><td>" + item.UpdateTime + "</td></tr>")
+                .parents("table")
+                .table("refresh");
+
+        });
+
+     
+    } else {
+        $('#deviceready').hide();
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. ");
+    }
+}
